6615062e7fa7b27e9fb7efbf53214f9c09a5663f
Marin Grubišić <marin.grubisic@gmail.com>
2026-03-05 09:06:23 +0100

    Update C:\GITHUB_Master\RPuG2026

--- Tjedan_2/T2_Zadatak_1.py
+++ Tjedan_2/T2_Zadatak_1.py
@@ -19,3 +19,39 @@
 #  NAJVITKIJI i koji je NAJKRAĆI (najmanja vitkoća).
 #
 # =============================================================================
+
+import math
+
+stupovi = [
+    {"oznaka": "S1", "D": 0.40, "L0": 4.50},    
+    {"oznaka": "S2", "D": 0.30, "L0": 6.00},    
+    {"oznaka": "S3", "D": 0.50, "L0": 3.50},    
+    {"oznaka": "S4", "D": 0.35, "L0": 7.50},    
+    {"oznaka": "S5", "D": 0.45, "L0": 5.00},    
+    ]
+
+# lambda_vrijednosti = []
+
+for s in stupovi:
+    A = math.pi * s["D"]**2 / 4
+    I = math.pi * s["D"]**4 / 64
+    i = (I / A)**0.5
+    lam = s["L0"] / i
+
+    # lambda_vrijednosti.append((s["oznaka"]), lam)
+
+    print(lam)
+    
+    if lam <= 25:
+        kategorija = "Kratki stup (izvijanje nije mjerodavno!)"
+        print("Kratki stup (izvijanje nije mjerodavno!)")
+    elif lam <= 50:
+        print("Umjereno vitak stup (paziti na P-delta efekte!)")
+    else:
+        print("Vrlo vitak stup (analiza P-delta OBAVEZNA!)")
+
+
+
+
+
+
